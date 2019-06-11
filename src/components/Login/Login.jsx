c1410ebe48129a2faed6633997c8429a706c3102
SandraGro <sandra.guerreroflores@gmail.com>
2019-06-11 11:26:16 -0500

    inicio de sesion

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,89 +1,93 @@
-import React from 'react';
-import Link from 'react-router-dom/Link';
-import { Form, Button, Modal } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom/';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
 
-/*function Login(props) {
-    return (
-        <div>
-            {props.name}
-        </div>
-    );
-}*/
+import { getDataWithQuery } from '../../utils/api';
+import storage from '../../utils/storage';
 
-/*
-const Login =  (props) =>{
-      return (
-        <div>
-            {props.name}
-        </div>
-    );
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            loginError: false,
+            loggedIn: false,
+        }
+
+        this.updateUsername = this.updateUsername.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        this.checkUser = this.checkUser.bind(this);
+        this.logIn = this.logIn.bind(this);
+    }
+
+    updateUsername({ target }) {
+        this.setState({ username: target.value, loginError: false });
+    }
+
+    updatePassword({ target }) {
+        this.setState({ password: target.value, loginError: false });
+    }
+
+    checkUser(userData) {
+        if (userData && userData.length) {
+            storage.store('user', userData[0]);
+            this.setState({ loggedIn: true });
+            console.log(storage.retreive('user'));
+        } else {
+            this.setState({ loginError: true });
+        }
+    }
+
+    logIn() {
+        getDataWithQuery(({ ...this.state }), 'users').then(this.checkUser);
+    }
+
+    render() {
+        let alert = null;
+        if (this.state.loggedIn) {
+            return (<Redirect to="/list" />);
+        }
+        if (this.state.loginError) {
+            alert = (<Alert variant="danger">Check password or username!</Alert>);
+        }
+        return (
+            <Modal.Dialog>
+                <Modal.Header>
+                    <Modal.Title>Log In</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group controlId="formUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Username"
+                            value={this.state.username}
+                            onChange={this.updateUsername}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.updatePassword}
+                        />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" type="button" onClick={this.logIn}>Log In</Button>
+                </Modal.Footer>
+                {alert}
+            </Modal.Dialog>
+        );
+    }
 }
-*/
-
-/*
-const Login =  ({name}) =>(
-    return (
-      <div>
-          {name}
-      </div>
-  );
-*/
-const Login = () => (
-    <Modal.Dialog>
-        <Modal.Header>
-            <Modal.Title>Log In</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicChecbox">
-                <Form.Check type="checkbox" label="Remember Me" />
-            </Form.Group>
-        </Modal.Body>
-        <Modal.Footer>
-            <Link to= "/list">
-            <Button variant="secondary" type="submit">
-                Submit
-              </Button>
-            </Link>
-        </Modal.Footer>
-    </Modal.Dialog>
-
-);
-
-// class Login extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             clicked: false,
-//             name: this.props.name,
-//         };
-//         this.changeClicked = this.changeClicked.bind(this);
-//     }
-//     changeClicked(event) {
-//         console.log(event.target);
-//         this.setState({clicked: !this.state.clicked})
-//     }
-//     render() {
-//         return (
-//             <div onClick={this.changeClicked}>
-//                 {this.state.name}
-//                 <br/>
-//                 {this.state.clicked.toString()}
-//             </div>
-//         )
-//     }
-// }
 
 export default Login;
